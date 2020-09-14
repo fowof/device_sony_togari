0e5ee0b71b9f057e1421040c674a4b32d3d5dbad
fowof <7818898+fowof@users.noreply.github.com>
2020-09-14 20:51:56 +0900

    update: add proprietary files to PRODUCT_COPY_FILES

--- togari.mk
+++ togari.mk
@@ -41,8 +41,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/configs/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sensor_def_qcomdev.conf \
-	 $(LOCAL_PATH)/configs/sensors_calib.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sensors_calib.conf
+   $(LOCAL_PATH)/configs/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sensor_def_qcomdev.conf
+
 
 # Thermal manager
 PRODUCT_COPY_FILES += \
@@ -53,3 +53,55 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/togari/togari-vendor.mk)
+
+# add missing proprietary files
+LOCAL_VENDOR_SONY_RHINE_COMMON := $(LOCAL_PATH)/repocopy/proprietary/vendor/sony/rhine-common/proprietary
+LOCAL_VENDOR_SONY_TOGARI       := $(LOCAL_PATH)/repocopy/proprietary/vendor/sony/togari/proprietary
+
+PRODUCT_COPY_FILES += \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/sensors_calib.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sensors_calib.conf \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b00:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b00 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b01:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b01 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b02:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b02 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b03:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b03 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b04:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b04 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b05:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b05 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b06:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b06 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b07:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b07 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b08:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b08 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b09:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b09 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b10:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b10 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.b11:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.b11 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/adsp.mdt:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/adsp.mdt \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.b00:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.b00 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.b01:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.b01 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.b02:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.b02 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.b03:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.b03 \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.flist:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.flist \
+	 $(LOCAL_VENDOR_SONY_RHINE_COMMON)/etc/firmware/tzhdcp.mdt:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/tzhdcp.mdt \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Hdmi_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Global_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Global_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Headset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Bluetooth_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_General_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_General_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Speaker_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Liquid/Liquid_Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Liquid/Liquid_Handset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_General_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_General_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Headset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Global_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Global_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Hdmi_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Handset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Bluetooth_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/Fluid/Fluid_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/Fluid/Fluid_Speaker_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Hdmi_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Hdmi_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Speaker_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Global_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Global_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Handset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Handset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_General_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_General_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Bluetooth_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/acdbdata/MTP/MTP_Headset_cal.acdb:$(TARGET_COPY_OUT_SYSTEM)/etc/acdbdata/MTP/MTP_Headset_cal.acdb \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/firmware/mba.b00:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/mba.b00 \
+	 $(LOCAL_VENDOR_SONY_TOGARI)/etc/firmware/mba.mdt:$(TARGET_COPY_OUT_SYSTEM)/etc/firmware/mba.mdt
+
+undefine LOCAL_VENDOR_SONY_RHINE_COMMON
+undefine LOCAL_VENDOR_SONY_TOGARI
