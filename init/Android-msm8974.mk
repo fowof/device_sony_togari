b504e03998c682dc6050d57d70f0122993c96f60
fowof <7818898+fowof@users.noreply.github.com>
2020-09-28 03:34:03 +0900

    add: files from device_sony_common

---
+++ init/Android-msm8974.mk
@@ -0,0 +1,22 @@
+ramdisk_init        := $(TARGET_RAMDISK_OUT)/init
+ramdisk_init_real   := $(TARGET_RAMDISK_OUT)/init.real
+ramdisk_init_sony   := $(TARGET_RAMDISK_OUT)/sbin/init_sony
+ramdisk_keycheck    := $(TARGET_RAMDISK_OUT)/sbin/keycheck
+ramdisk_toybox_init := $(TARGET_RAMDISK_OUT)/sbin/toybox_init
+
+# If /init is a file and not a symlink then rename it to /init.real
+# and make /init be a symlink to /sbin/init_sony (which will execute
+# /init.real, if appropriate.
+$(ramdisk_init_real): \
+	$(ramdisk_init) \
+	$(ramdisk_init_sony) \
+	$(ramdisk_keycheck) \
+	$(ramdisk_toybox_init)
+	if [ ! -L $(ramdisk_init) ]; then \
+	  mv $(ramdisk_init) $(ramdisk_init_real); \
+	  ln -s /sbin/init_sony $(ramdisk_init); \
+	else \
+	  echo "init.real: $(ramdisk_init) is already a symlink"; \
+	fi
+
+ALL_DEFAULT_INSTALLED_MODULES += $(ramdisk_init_real)
