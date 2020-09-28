2d2a4a5aecf94eaba2258e3589b97502afa432a8
fowof <7818898+fowof@users.noreply.github.com>
2020-09-29 00:16:28 +0900

    update: indent equals

--- modules/camera/Android.mk
+++ modules/camera/Android.mk
@@ -1,14 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := \
     CameraWrapper.cpp
-
 LOCAL_C_INCLUDES := \
     framework/native/include \
     system/media/camera/include
-
 LOCAL_SHARED_LIBRARIES := \
     libhardware \
     liblog \
@@ -18,12 +15,9 @@ LOCAL_SHARED_LIBRARIES := \
     libsensor \
     libutils \
     android.hidl.token@1.0-utils
-
 LOCAL_STATIC_LIBRARIES := \
     libarect
-
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := camera.qcom
-LOCAL_MODULE_TAGS := optional
-
+LOCAL_MODULE               := camera.qcom
+LOCAL_MODULE_TAGS          := optional
 include $(BUILD_SHARED_LIBRARY)
