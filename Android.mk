dd841d5bedf4d2cbb2675cbdd729bd5de37cc57b
fowof <7818898+fowof@users.noreply.github.com>
2020-09-28 18:33:11 +0900

    udpate: not to create links to wcd9320 firmwares

--- Android.mk
+++ Android.mk
@@ -1,22 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR_PLATFORM),rhine)
-    include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-    $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
-        ln -sf /data/misc/audio/wcd9320_anc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
-        ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
-        ln -sf /data/misc/audio/mbhc.bin \
-        $(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
-endif
-
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
+ifeq ($(BOARD_VENDOR_PLATFORM),rhine)
+
+# include $(call all-subdir-makefiles,$(LOCAL_PATH))
+#
+# $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9320; \
+# 		ln -sf /data/misc/audio/wcd9320_anc.bin \
+# 		$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_anc.bin; \
+# 		ln -sf /data/misc/audio/wcd9320_mad_audio.bin \
+# 		$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mad_audio.bin; \
+# 		ln -sf /data/misc/audio/mbhc.bin \
+# 		$(TARGET_OUT_ETC)/firmware/wcd9320/wcd9320_mbhc.bin)
+
 ifeq ($(TARGET_DEVICE),togari)
 #  include $(call first-makefiles-under,$(LOCAL_PATH))
 	include $(call all-subdir-makefiles)
+endif
 endif
 endif
 endif
