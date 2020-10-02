6b48264895d49737f3e4ab625fbb2d8e81dbecdc
fowof <7818898+fowof@users.noreply.github.com>
2020-10-03 04:58:16 +0900

    update: include default aosp modules for display, audio, media, gps, bt and wlan

--- Android.mk
+++ Android.mk
@@ -17,6 +17,22 @@ ifeq ($(BOARD_VENDOR_PLATFORM),rhine)
 ifeq ($(TARGET_DEVICE),togari)
 #  include $(call first-makefiles-under,$(LOCAL_PATH))
 	include $(call all-subdir-makefiles)
+
+	# HALs
+	# include hardware/qcom/display/msm8994/Android.mk
+	# include $(call all-makefiles-under,hardware/qcom/audio)
+	# include $(call all-makefiles-under,hardware/qcom/media/msm8974)
+	# include $(call all-makefiles-under,hardware/qcom/gps/msm8994)
+	# include $(call all-makefiles-under,hardware/qcom/bt/msm8960)
+	# include $(call all-makefiles-under,hardware/qcom/wlan/qcwcn)
+
+	include hardware/qcom/display/msm8974/Android.mk
+	include hardware/qcom/audio/Android.mk
+	include hardware/qcom/media/Android.mk
+	include $(call all-makefiles-under,hardware/qcom/gps/msm8974)
+	include $(call all-makefiles-under,hardware/qcom/bt/msm8960)
+	include $(call all-makefiles-under,hardware/qcom/wlan/qcwcn)
+
 endif
 endif
 endif
