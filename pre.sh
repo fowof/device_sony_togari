5236ad41bc0775682d50ddd1f12f44a753bfe3ee
fowof <7818898+fowof@users.noreply.github.com>
2021-04-28 23:06:18 +0900

    udpate:

--- pre.sh
+++ pre.sh
@@ -1,22 +1,14 @@
 
-#
-# `source` this file
-#
+###                                                     ###
+### `source` this file after `source` build/envsetup.sh ###
+###                                                     ###
 
-cd "$(dirname "$0")"
-DEVICE_PATH="$(pwd)"
+_DIR_DEVICE="$(cd "$(dirname "$0")"; pwd)"
+_DIR_REPOROOT="$(cd "$_DIR_DEVICE";cd ../../../;pwd)"
 
-cd ../../../
-DEVICE_PATH=${DEVICE_PATH#$(pwd)/}
-
-#
-# COPY FILES
-#
-
-for file in $(find $DEVICE_PATH/repocopy -type f)
-do
-  cp $file ${file#$DEVICE_PATH/repocopy/}
-done
+[ "$(pwd)" = "$_DIR_REPOROOT"     ] || cd "$_DIR_REPOROOT"
+[ -f build/envsetup.sh            ] || return 1
+command -v repopick >/dev/null 2>&1 || source build/envsetup.sh
 
 #
 # PICK PATCHES
@@ -31,3 +23,12 @@ repopick -g https://gerrit.aicp-rom.com 105646 -P frameworks/native
 repopick -g https://gerrit.aicp-rom.com 105644 -P build/make
 repopick -g https://gerrit.aicp-rom.com 105645 -P build/soong
 repopick -g https://gerrit.aicp-rom.com 105817 -P vendor/lineage
+
+#
+# COPY FILES
+#
+
+for file in $(find ${_DIR_DEVICE}/repocopy -type f)
+do
+  cp $file ${file#${_DIR_DEVICE}/repocopy/}
+done
