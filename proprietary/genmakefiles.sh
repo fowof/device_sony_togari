72b29ce7801536a411a6b1a994fb900e71804f1c
fowof <7818898+fowof@users.noreply.github.com>
2021-04-29 06:57:15 +0900

    update:

--- proprietary/genmakefiles.sh
+++ proprietary/genmakefiles.sh
@@ -12,10 +12,13 @@ cd "$subdir"
 [ -e "Android.mk"  ] && dumpandroidmk=false || dumpandroidmk=true
 [ -e "packages.mk" ] && dumppkgsmk=false    || dumppkgsmk=true
 
+if $dumpandroidmk || $dumppkgsmk
+then
+
 $dumpandroidmk && echo 'LOCAL_PATH := $(call my-dir)' > Android.mk
 $dumppkgsmk    && echo > packages.mk
 
-for file in $(find . -type f -not -name Android.mk -not -name packages.mk | sort)
+for file in $(find . -type f -not -name Android.mk -not -name packages.mk -not -name '.*' | sort)
 do
 
 file=${file#\./}
@@ -24,23 +27,43 @@ name=$(basename "${file}")
 stem=${name%\.*}
 suffix=${file#*\.}
 
+out='$(TARGET_OUT)'
+while true
+do
+[ "${path#vendor/lib/}" != "$path" -o "$path" = 'vendor/lib' ] && { path="${path#vendor/lib}"; out='$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)'; break; }
+[ "${path#vendor/}"     != "$path" -o "$path" = 'vendor'     ] && { path="${path#vendor}"    ; out='$(TARGET_OUT_VENDOR)'; break; }
+[ "${path#etc/}"        != "$path" -o "$path" = 'etc'        ] && { path="${path#etc}"       ; out='$(TARGET_OUT_ETC)'   ; break; }
+break
+done
+path=${path#/}
+
+module=${stem}_${suffix}
+if [ -n "$path" ]; then
+  module=$(echo $path | sed 's/[\/\.]/_/g')_${module}
+  path=${out}/${path}
+else
+  path=${out}
+fi
+
 $dumpandroidmk && cat << EOF >> Android.mk
 
 include \$(CLEAR_VARS)
-LOCAL_MODULE        := ${stem}_${suffix}
+LOCAL_MODULE        := ${module}
 LOCAL_MODULE_OWNER  := Sony Mobile
 LOCAL_SRC_FILES     := ${file}
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_STEM   := ${stem}
 LOCAL_MODULE_SUFFIX := .${suffix}
 LOCAL_MODULE_CLASS  := ETC
-LOCAL_MODULE_PATH   := \$(TARGET_OUT)/${path}
+LOCAL_MODULE_PATH   := ${path}
 include \$(BUILD_PREBUILT)
 EOF
 
-$dumppkgsmk && echo "PRODUCT_PACKAGES += ${stem}_${suffix}" >> packages.mk
+$dumppkgsmk && echo "PRODUCT_PACKAGES += ${module}" >> packages.mk
 
 done
+
+fi
 
 cd ../
 
