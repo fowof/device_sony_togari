c987569c359fa7934f9bc94f2ed7bc67f65a30b1
fowof <7818898+fowof@users.noreply.github.com>
2021-12-19 21:55:41 +0900

    udpate: delete pre.sh and move repocopy script to repocopy/

---
+++ repocopy/run.sh
@@ -0,0 +1,18 @@
+
+_DIR_DEVICE="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; cd ../; pwd)"
+_DIR_REPOROOT="$(cd "$_DIR_DEVICE"; cd ../../../; pwd)"
+
+[ "$(pwd)" = "$_DIR_REPOROOT"     ] || cd "$_DIR_REPOROOT"
+
+_DIR_DEVICE=${_DIR_DEVICE#$_DIR_REPOROOT}
+_DIR_DEVICE=${_DIR_DEVICE#/}
+
+for src in $(find ${_DIR_DEVICE}/repocopy -type f -mindepth 2)
+do
+  dst="${src#${_DIR_DEVICE}/repocopy/}"
+  if [ -s "$src" ]; then
+    cp "$src" "$dst"
+  else
+    [ -f "$dst" ] && rm "$dst"
+  fi
+done
